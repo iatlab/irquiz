fbbf77aff692924d67bf1930f32c39ff9598a14c
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2017-02-07 12:07:53 +0900

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 bundle exec jekyll b -s src
 git checkout gh-pages
-cp _site/* ./
-git add .
+cp _site/index.html ./index.html
+git add ./index.html
 git commit -m "Update: $1"
 git push origin gh-pages
 git checkout pages
