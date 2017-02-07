c2ff7fac30efd870f177b4b97a0b6dc43041963b
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2017-02-07 12:10:10 +0900

    Update build, again

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
+git commit src/index.html -m "Update: $1"
 bundle exec jekyll b -s src
 git checkout gh-pages
 cp _site/index.html ./index.html
-git add ./index.html
-git commit -m "Update: $1"
+git commit ./index.html -m "Update: $1"
 git push origin gh-pages
 git checkout pages
